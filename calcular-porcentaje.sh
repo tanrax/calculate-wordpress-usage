63f74932c5420e19a6422a81b5cd9a07fe9b73c3
Andros Fenollosa <tanrax@users.noreply.github.com>
2019-10-31 17:44:48 +0100

    Script calculate

---
+++ calcular-porcentaje.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CSV="resources/top-1m.csv"
+NUM_WORDPRESS=$(cat $CSV | grep ,true$ | wc -l)
+NUM_NOT_WORDPRESS=$(cat $CSV | grep ,false$ | wc -l)
+NUM_TIMEOUT=$(cat $CSV | grep ,timeout$ | wc -l)
+TOTAL=$(cat $CSV | wc -l)
+
+echo "($NUM_WORDPRESS * 100) / ($TOTAL - $NUM_TIMEOUT)" | bc
